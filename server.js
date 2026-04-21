b9b28c6314832e919dc69b898ed376ab274c57a0
Zafir Hassan <zafirhassan@gmail.com>
2026-04-22 00:13:08 +0500

    Cors Fixed

--- server.js
+++ server.js
@@ -16,10 +16,13 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: 'https://front-end-lease-together.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   })
 );
+app.options('*', cors());
 app.use(express.json());
 app.use(cookieParser());
 
